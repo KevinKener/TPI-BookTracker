96dcf01dc617e3ed6975de4f93676debe05dfc18
MDG-22 <116525431+MDG-22@users.noreply.github.com>
2025-06-02 01:08:56 -0300

    admin-page

--- Book-Tracker/src/components/adminModal/AdminModal.jsx
+++ Book-Tracker/src/components/adminModal/AdminModal.jsx
@@ -1,22 +1,39 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Modal, Button, ModalHeader, ModalBody, ModalFooter, FormSelect } from 'react-bootstrap';
 import { useTranslate } from '../hooks/translation/UseTranslate';
 import { AuthenticationContext } from '../services/auth.context';
 import './adminModal.css'
 
-const AdminModal = ({ user, showModal, closeModal }) => {
+const AdminModal = ({ user, showEditModal, closeModal, updateUser, newUsername, newEmail, newRole, setNewUsername, setNewEmail, setNewRole }) => {
 
   const translate = useTranslate();
   const { role } = useContext(AuthenticationContext);
 
   // TRAER CONTEXT ROLE PARA OTORGAR ROLE
 
+  const handleUpdateUser = () => {
+    updateUser(user.id);
+  }
+
+  const handleNewUsername = (event) => {
+    setNewUsername(event.target.value)
+  }
+
+  const handleNewEmail = (event) => {
+    setNewEmail(event.target.value)
+  }
+
+  const handleNewRole = (event) => {
+    setNewRole(event.target.value)
+  }
+
+
   return (
 
     // e-u- prefijo para clases del componente
-        <Modal show={showModal} onHide={closeModal} centered size='md' >
-        <ModalHeader closeButton className='e-u-header'>{translate("edit_user")}</ModalHeader>
-        <ModalBody className='e-u-body'>
+        <Modal show={showEditModal} onHide={closeModal} centered size='md' >
+        <ModalHeader closeButton className='admin-modal-header'>{translate("edit_user")}</ModalHeader>
+        <ModalBody className='admin-modal-body'>
           <div className="modal-body">
 
           <div className="edit-title">
@@ -24,6 +41,8 @@ const AdminModal = ({ user, showModal, closeModal }) => {
           </div>
           <input className='edit-input'
             placeholder={user.username}
+            value={newUsername}
+            onChange={handleNewUsername}
             />
           <br />
           <div className="edit-title">
@@ -31,26 +50,21 @@ const AdminModal = ({ user, showModal, closeModal }) => {
           </div>
           <input className='edit-input'
             placeholder={user.email}
+            value={newEmail}
+            onChange={handleNewEmail}
             />
           <br />
           <div className="edit-title">
             {translate("role")} <span className='actual-role'>(actual: {user.role})</span>
           </div>
-          <FormSelect className='edit-select'>
-            <option value=""></option>
+          <FormSelect className='edit-select' value={newRole} onChange={handleNewRole}>
+            <option></option>
             <option value="reader">{translate("reader")}</option>
             <option value="admin">{translate("admin")}</option>
             { role === "mod" &&
               <option value="mod">{translate("mod")}</option>
             }
           </FormSelect>
-          <br />
-          <div className="delete-btn">
-
-          <Button variant='danger'>
-            Eliminar usuario
-          </Button>
-          </div>
 
             </div>
         </ModalBody>
@@ -58,7 +72,7 @@ const AdminModal = ({ user, showModal, closeModal }) => {
             <Button variant='secondary' onClick={closeModal} >
                 {translate("cancel")}
             </Button>
-            <Button variant='success' onClick={closeModal} >
+            <Button variant='success' onClick={handleUpdateUser} >
                 {translate("save_changes")}
             </Button>
         </ModalFooter>
